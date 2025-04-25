00acba4706d069a8b4dc597ca51cde86caf745b7
Shamsa <falolashamsa@gmail.com>
2025-04-25 04:23:18 +0100

    Editted lab5

--- python_labwork/firstlab/lab5.py
+++ python_labwork/firstlab/lab5.py
@@ -1,5 +1,19 @@
 num_one = float(input("Enter first number: "))
 num_two = float(input("Enter second number: "))
-sum_num = num_one + num_two
 
-print(sum_num)
+print("Choose operation: +, -, *, /")
+op = input("Enter operation: ")
+
+if op == '+':
+    print("Result:", num_one + num_two)
+elif op == '-':
+    print("Result:", num_one - num_two)
+elif op == '*':
+    print("Result:", num_one * num_two)
+elif op == '/':
+    if num_two != 0:
+        print("Result:", num_one / num_two)
+    else:
+        print("Error! Division by zero.")
+else:
+    print("Invalid operation")
