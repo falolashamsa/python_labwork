90a89e8b1158937c4db8ce0a2c0692ba5c5b6dc5
Shamsa <falolashamsa@gmail.com>
2025-04-26 07:04:09 +0100

    Exception handling in lab 5

--- python_labwork/firstlab/lab5.py
+++ python_labwork/firstlab/lab5.py
@@ -1,19 +1,23 @@
-num_one = float(input("Enter first number: "))
-num_two = float(input("Enter second number: "))
+try:
+    num_one = float(input("Enter first number: "))
+    num_two = float(input("Enter second number: "))
 
-print("Choose operation: +, -, *, /")
-op = input("Enter operation: ")
+    print("Choose operation: +, -, *, /")
+    op = input("Enter operation: ")
 
-if op == '+':
-    print("Result:", num_one + num_two)
-elif op == '-':
-    print("Result:", num_one - num_two)
-elif op == '*':
-    print("Result:", num_one * num_two)
-elif op == '/':
-    if num_two != 0:
-        print("Result:", num_one / num_two)
+    if op == '+':
+        print("Result:", num_one + num_two)
+    elif op == '-':
+        print("Result:", num_one - num_two)
+    elif op == '*':
+        print("Result:", num_one * num_two)
+    elif op == '/':
+        if num_two != 0:
+            print("Result:", num_one / num_two)
+        else:
+            print("Error! Division by zero.")
     else:
-        print("Error! Division by zero.")
-else:
-    print("Invalid operation")
+        print("Invalid operation")
+except ValueError as e:
+    # print(e) an additonal way to handle exceptions
+    print("Invalid user input")
