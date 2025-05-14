b1c1427df1e926c09ed89eed8223008ce38c0975
Shamsa <falolashamsa@gmail.com>
2025-05-14 22:40:33 +0100

    created second  lab

---
+++ python_labwork/secondlab/lab2.py
@@ -0,0 +1,11 @@
+price = 1000000
+budget = float(input("Enter your budgeted amount: "))
+
+if budget < 500000 and budget > 300000:
+    down_payment = (10 / 100) * price
+    print(down_payment)
+
+elif budget > 500000:
+    down_payment = (20 / 100) * price
+    print(down_payment)
+
